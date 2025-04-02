a47d8a81501821f5f5e9ceac09bec25c5bb52fcd
54ntu <chaudharysantaram8@gmail.com>
2025-04-02 16:42:36 +0545

    project is started

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import AppDataSource from "./config/db.config"
+
+
+
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })

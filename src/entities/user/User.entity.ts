8901c0761cd7e53810e5e64d788bc4d90fe2952c
54ntu <chaudharysantaram8@gmail.com>
2025-04-02 19:17:05 +0545

    user entity is created

---
+++ src/entities/user/User.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity } from "typeorm";
+import { CommonEntity } from "../common/Common.entity";
+
+@Entity()
+export class User extends CommonEntity {
+    @Column({ unique: true, length: 50 })
+    email: string
+
+
+    @Column({ length: 50 })
+    password: string
+
+    @Column()
+    name: string
+
+    @Column()
+    phone: string
+}
